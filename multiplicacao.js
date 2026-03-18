51e92fe5e09cd0084b90c59af28c53afb1919f78
Alice <lilsp0624@gmail.com>
2026-03-18 10:17:23 -0300

    Adicionar coverage

--- multiplicacao.js
+++ multiplicacao.js
@@ -1,4 +1,7 @@
 function multiplicacao(a, b){
+    if(a == 0){
+        return 0;
+    }
     return a * b;
 }
 
--- multiplicacao.test.js
+++ multiplicacao.test.js
@@ -4,5 +4,9 @@ test('multiplicacao de dois valores', () => {
   expect(multiplicacao(2, 4)).toBe(8);
 });
 
+test('multiplicacao de dois valores, em que um é 0', () => {
+  expect(multiplicacao(0, 4)).toBe(0);
+});
+
 // arquivo de teste
 // Testando git branch
