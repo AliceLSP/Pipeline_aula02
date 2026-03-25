dde65cb473a62a704e5537e64cdd649713207c44
Alice <lilsp0624@gmail.com>
2026-03-25 10:25:43 -0300

    teste variavel de segurança

--- src/multiplicacao.js
+++ src/multiplicacao.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 function multiplicacao(a, b){
+    const password = "funcao_multiplicacao";
     return a * b;
 }
 
 function soma(a, b) { 
     let resultado = a + b; 
-    let x = 10; // variável inútil 
+    let x = 10; // variável inútil
+    let pass = process.env.PASSWORD; 
     return resultado; 
 }
 
