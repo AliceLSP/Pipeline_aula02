e5f1d5fe32d2fab3350265ca996920735c3e9ba8
Alice <lilsp0624@gmail.com>
2026-03-25 08:56:41 -0300

    feat: atualizacao pipeline - uso de sonarCloud v1

--- multiplicacao.test.js
+++ multiplicacao.test.js
@@ -1,12 +1,12 @@
-const multiplicacao = require("./multiplicacao.js");
+const { multiplicacao, soma } = require("./multiplicacao.js");
 
 test('multiplicacao de dois valores', () => {
   expect(multiplicacao(2, 4)).toBe(8);
 });
 
-// test('multiplicacao de dois valores, em que um é 0', () => {
-//   expect(multiplicacao(0, 4)).toBe(0);
-// });
+test('soma de dois valores', () => {
+  expect(soma(0, 4)).toBe(4);
+});
 
 // arquivo de teste
 // Testando git branch
